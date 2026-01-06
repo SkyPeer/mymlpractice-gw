4646f0e191dc27e18bebce7a7dc6dde9a14b917c
Oleg Selivantsev <o.selivancev@gmail.com>
2026-01-07 00:53:11 +0400

    dto updated

--- src/forecast/dto/updateModel.dto.ts
+++ src/forecast/dto/updateModel.dto.ts
@@ -1,6 +1,6 @@
 export class UpdateModelDto {
   readonly epochs: number;
   readonly batchSize: number;
-  // readonly model_name: string;
+  readonly model_name: string;
   readonly description: string;
 }
