b291610843b1971a2ef0324380cbfd0c026f408a
Oleg Selivantsev <o.selivancev@gmail.com>
2026-01-06 04:38:03 +0400

    added new endpoints

--- src/forecast/forecast.controller.ts
+++ src/forecast/forecast.controller.ts
@@ -12,12 +12,10 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
-import { ArticleService } from '@app/article/article.service';
 import { AuthGuard } from '@app/user/guards/auth.guard';
-import { User } from '@app/user/decorators/user.decorator';
-import { UserEntity } from '@app/user/user.entity';
 import { ForecastService } from '@app/forecast/forecast.service';
-import { LoadModelService } from '@app/forecast/forecast.loadModel.service';
+import { LoadModelService } from '@app/forecast/forecast.loadModel';
+import { CreatModelDto } from '@app/forecast/dto/createModel.dto';
 
 @Controller('forecast')
 export class ForecastController {
@@ -30,21 +28,38 @@ export class ForecastController {
   @Header('Cache-Control', 'no-store')
   // @UseGuards(AuthGuard)
   async getInitialData() {
-    return await this.forecastService.getSeasonsData();
+    // const data = await this.forecastService.getSeasonsData();
+    // console.log('data', data);
+    return await this.forecastService.getSourceData();
   }
 
   @Get('/train')
   @Header('Cache-Control', 'no-store')
   // @UseGuards(AuthGuard)
-  async training() {
-    return this.forecastService.trainModel();
+  async trainModel(@Body('modelId') modelId: number) {
+    // return this.forecastService.trainModel(modelName);
+    return modelId;
+  }
+
+  @Post('/model')
+  @Header('Cache-Control', 'no-store')
+  // @UseGuards(AuthGuard)
+  async createModel(@Body('modelParams') modelParams: CreatModelDto) {
+    return await this.forecastService.createNewModel(modelParams);
+  }
+
+  @Post('/retrain')
+  @Header('Cache-Control', 'no-store')
+  // @UseGuards(AuthGuard)
+  async reTrainModel(@Body('id') id: number) {
+    return this.forecastService.retrainModel(id);
   }
 
   @Get('/predict')
   @Header('Cache-Control', 'no-store')
   // @UseGuards(AuthGuard)
-  async predict() {
-    return this.forecastService.predict();
+  async predict(modelId) {
+    return this.forecastService.predict(modelId);
   }
 
   @Get('/model')
