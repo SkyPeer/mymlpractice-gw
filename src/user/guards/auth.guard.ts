e9fad688d074c127cf472abb74fbbf76bca6f574
Oleg Selivantsev <o.selivancev@gmail.com>
2026-08-17 02:24:18 +0400

    auth.guard.ts codestyle

--- src/user/guards/auth.guard.ts
+++ src/user/guards/auth.guard.ts
@@ -1,15 +1,23 @@
-import {Injectable, ExecutionContext, HttpException, HttpStatus, CanActivate} from '@nestjs/common';
-import {ExpressRequestInterface} from "@app/types/expressRequest.interface";
+import {
+  Injectable,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+  CanActivate,
+} from '@nestjs/common';
+import { ExpressRequestInterface } from '@app/types/expressRequest.interface';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    canActivate(context: ExecutionContext): boolean {
-        const request = context.switchToHttp().getRequest<ExpressRequestInterface>();
+  canActivate(context: ExecutionContext): boolean {
+    const request = context
+      .switchToHttp()
+      .getRequest<ExpressRequestInterface>();
 
-        if(request.user) {
-            return true;
-        }
-
-        throw new HttpException('Not authorized', HttpStatus.UNAUTHORIZED);
+    if (request.user) {
+      return true;
     }
+
+    throw new HttpException('Not authorized', HttpStatus.UNAUTHORIZED);
+  }
 }
